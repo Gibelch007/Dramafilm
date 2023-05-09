04c3f7b816b5966cde99d3dafd7a36016b6b3df3
gibelc007 <giovanaferrodrigues@gmail.com>
2023-05-09 08:43:17 -0300

    Descrever as alterações realizadas no projeto

--- Dramafilm/Services/DramaService.cs
+++ Dramafilm/Services/DramaService.cs
@@ -1,12 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Dramafilm.Models;
 
-namespace Dramafilm.Services
-{
+namespace Dramafilm.Services;
+
     public class DramaService
     {
-        
+      private readonly IHttpContextAccessor _session;
+      private readonly string Generofile = @"Data\Genero.json";
+      private readonly string Seriesfile = @"Data\Series.json";
+     
+      public DramaService (IHttpContextAccessor session)
+      {
+          _session = session;
+          PopularSessao();
+      }
+
+      public List<Genero> GetGenero();
+      {
+          PopularSessao();
+          var genero = JsonSerializer.Deserialize<List<Genero>>
+          (_session.HttpContext.session.GetString("Genero"));
+          Return genero;
+      }
+      public List<Series> GetSeries();
+      {
+          PopularSessao();
+          var Series = JsonSerializer.Deserialize<List<Series>>
+            (_session.HttpContext.session.GetString("Series"));   
+            Return series;
+      }
+      
+
     }
-}
--- Dramafilm/Services/IFilmeService.cs
+++ Dramafilm/Services/IFilmeService.cs
@@ -1,13 +1,12 @@
 using Dramafilm.Models;
-using Dramafilm.Services;
-
-namespace Dramafilm.Services
-{
+namespace Dramafilm.Services;
     public interface IFilmeServices
     {
         List<Series> GetSeries();
         List<Genero> GetGenero();
         Genero GetGenero(int Numero);
+        DramafilmDto GetDramafilmDto();
+        DetaisDto GetDetaisDto();
+        Genero GetGenero(string Nome);
 
     }
-}
